b3f317071f7c4e7663b33e88e0cd9698d328b3c5
luarakelly <lua.kellly@gmail.com>
2026-03-31 22:00:52 +0300

    Define application routes with React Router (home, order tracker, menu, combi builder, cart, user profile, admin panel, login/register)

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,12 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/home.tsx"),
+  route("adm-panel", "routes/AdmPanel.tsx"),
+  route("order-tracker", "routes/OrderTracker.tsx"),
+  route("user-profile", "routes/UserProfile.tsx"),
+  route("menu", "routes/Menu.tsx"),
+  route("combo-builder", "routes/ComboBuilder.tsx"),
+  route("cart", "routes/Cart.tsx"),
+  route("login", "routes/Login.tsx"),
+] satisfies RouteConfig;
