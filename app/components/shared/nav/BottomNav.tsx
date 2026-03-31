eb624382bb7c22911d5debdab0f30ba517cbdef9
luarakelly <lua.kellly@gmail.com>
2026-03-31 23:43:35 +0300

    feat(bottom-nav): add mobile BottomNav with same logic as Navbar, but mobile friendly version.

--- app/components/shared/nav/BottomNav.tsx
+++ app/components/shared/nav/BottomNav.tsx
@@ -1,3 +1,30 @@
+import { Link, useLocation } from "react-router";
+import "./nav.css";
+
 export default function BottomNav() {
-  return <h1>Bottom Navigation</h1>;
+  const location = useLocation();
+  const isAdmin = location.pathname.startsWith("/adm");
+
+  return (
+    <nav className="bottom-nav">
+      {!isAdmin && (
+        <>
+          <Link to="/menu" className="bottom-link">🍱</Link>
+          <Link to="/combo-builder" className="bottom-link">🥢</Link>
+        </>
+      )}
+      
+      <Link to={isAdmin ? "/adm-panel" : "/"} className="logo">🏠</Link>
+
+      {isAdmin ? 
+        (
+            <Link to="/adm-profile" className="nav-icon">👤</Link>
+        ) : (
+            <>
+                <Link to="/login" className="nav-icon">👤</Link>
+                <Link to="/cart" className="nav-icon">🛒</Link>
+            </>
+        )}
+    </nav>
+  );
 }
