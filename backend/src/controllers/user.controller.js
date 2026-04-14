9bf1b85b6b76513dedb255d9d2b602e685bd0710
luarakelly <lua.kellly@gmail.com>
2026-04-14 17:55:48 +0300

    Add controllers placeholder in order to test the routes endpoints.

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,6 @@
+import { placeholder } from '../utils/paceholder.js';
+
+export const getProfile = placeholder('users.getProfile');
+export const updateProfile = placeholder('users.updateProfile');
+export const getRewards = placeholder('users.getRewards');
+export const getStamps  = placeholder('users.getStamps');
