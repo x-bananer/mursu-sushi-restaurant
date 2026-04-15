41dbaba47e946a4d0d250f63800896c3bb23d13f
luarakelly <lua.kellly@gmail.com>
2026-04-15 15:41:37 +0300

    Add cart place holder

---
+++ backend/src/controllers/cart.controller.js
@@ -0,0 +1,5 @@
+import { placeholder } from '../utils/paceholder.js';
+
+export const get                = placeholder('orders.get');
+export const create             = placeholder('orders.create');
+export const update             = placeholder('orders.cancel');
