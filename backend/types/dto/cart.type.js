2607f29264762b24d7cc55bc0271e26e13989659
Kseniia Shlenskaia <xeniya.shlyonskaya@gmail.com>
2026-04-15 13:14:39 +0300

    Add cart DTO

--- backend/types/dto/cart.type.js
+++ backend/types/dto/cart.type.js
@@ -1,4 +1,41 @@
 /**
- * TODO
- * Define CartDTO after cart data model is confirmed.
+ * @typedef {Object} CartDTO
+ * @property {number} id // from Carts table
+ * @property {import('./user.type.js').UserDTO | null} user
+ * @property {string | null} session_id
+ * @property {number} total_price
+ * @property {Date} created_at
+ * @property {Date} updated_at
+ * @property {Array<DishCartItemDTO | CustomCartItemDTO>} items
+ */
+
+/**
+ * @typedef {Object} DishCartItemDTO
+ * @property {number} id // from CartItems table
+ * @property {number} cart_id
+ * @property {import('./dish.type.js').DishDTO} dish
+ * @property {number} quantity
+ * @property {number} price
+ * @property {import('../common.type.js').OrderItemType} type
+ * @property {null} ingredients
+ */
+
+/**
+ * @typedef {Object} CustomCartItemDTO
+ * @property {number} id // from CartItems table
+ * @property {number} cart_id
+ * @property {null} dish
+ * @property {string} name
+ * @property {number} quantity
+ * @property {number} price
+ * @property {import('../common.type.js').OrderItemType} type
+ * @property {Array<CustomCartIngredientDTO>} ingredients
+ */
+
+/**
+ * @typedef {Object} CustomCartIngredientDTO
+ * @property {number} id // from CartItemIngredients table
+ * @property {import('./ingredient.type.js').IngredientDTO} ingredient
+ * @property {number} quantity
+ * @property {number} position
  */
