c26c2778cd840fc4b3dc8d527ccc6c3a403942c1
luarakelly <lua.kellly@gmail.com>
2026-04-16 21:41:06 +0300

    Fix IDE complainign about type

---
+++ backend/src/models/db/connection.js
@@ -0,0 +1,16 @@
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.SUSHI_RESTAURANT_DB_USER,
+  password: process.env.SUSHI_RESTAURANT_DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  multipleStatements: true,
+});
