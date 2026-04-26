3cce5633e6957a0f7987f1f7fd120997edc743d1
Kseniia Shlenskaia <xeniya.shlyonskaya@gmail.com>
2026-04-26 12:40:25 +0300

    Fix controller to one method

--- backend/src/controllers/payment.controller.js
+++ backend/src/controllers/payment.controller.js
@@ -1,62 +1,17 @@
 import * as paymentService from '../services/integrations/payment.service.js';
-import * as cartService from '../services/cart/cart.service.js';
 
 export async function initiate(req, res, next) {
 	try {
 		// TODO: get userId from request when auth is ready
 		const userId = 2;
-		const cart = await cartService.getCartByUserId(userId);
-
-		if (!cart || !cart.items.length) {
-			return res.status(400).json({ message: 'Cart is empty' });
-		}
-
-		const payment = await paymentService.initiateMobilePayPayment(userId, cart.total_price);
-		res.json({ payment });
-	} catch (err) {
-		next(err);
-	}
-}
-
-export async function confirm(req, res, next) {
-	try {
-		const paymentId = Number(req.params.id);
-		if (Number.isNaN(paymentId)) {
-			return res.status(400).json({ message: 'Invalid payment id' });
-		}
-
-		// TODO: get userId from request when auth is ready
-		const userId = 2;
-		const { delivery_type_id, address } = req.body;
-
-		const order = await cartService.checkoutCartByUserId(userId, {
+		const { delivery_type_id, address, payment_result } = req.body;
+		const result = await paymentService.payWithMobilePay(userId, {
 			delivery_type_id,
 			address,
+			payment_result,
 		});
 
-		await paymentService.attachOrderToPayment(paymentId, userId, order.id);
-		
-		const payment = await paymentService.confirmMobilePayPayment(paymentId, userId);
-		
-		await cartService.clearCartByUserId(userId);
-
-		res.json({ order, payment });
-	} catch (err) {
-		next(err);
-	}
-}
-
-export async function fail(req, res, next) {
-	try {
-		const paymentId = Number(req.params.id);
-		if (Number.isNaN(paymentId)) {
-			return res.status(400).json({ message: 'Invalid payment id' });
-		}
-
-		// TODO: get userId from request when auth is ready
-		const userId = 2;
-		const payment = await paymentService.failMobilePayPayment(paymentId, userId);
-		res.json({ payment });
+		res.json(result);
 	} catch (err) {
 		next(err);
 	}
