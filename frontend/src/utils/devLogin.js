16f6ea4a6ac10d05e4f84f6f0ed70123118b8238
luarakelly <lua.kellly@gmail.com>
2026-05-01 14:06:51 +0300

    Add a development module to facilitate login od dev account for testing purposes, must be deleted once tests are done. Currently provide access to the user token so we can verify the orders this user has.

---
+++ frontend/src/utils/devLogin.js
@@ -0,0 +1,26 @@
+import { fetchData } from "./fetchData";
+
+export default async function devLogin() {
+  try {
+    const data = await fetchData("/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: "dev@example.com",
+        password: "password123",
+      }),
+    });
+
+    // store token
+    localStorage.setItem("token", data.token);
+	localStorage.setItem("user", data.user);
+
+    console.log("✅ Logged in dev user:", data);
+
+    return data;
+  } catch (err) {
+    console.error("❌ Login failed:", err.message);
+  }
+}
