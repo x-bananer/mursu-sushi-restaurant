c14f5384d8c9c22422d0420a5680edc82a834eb7
luarakelly <lua.kellly@gmail.com>
2026-05-05 11:40:34 +0300

    Add a reusable table base component that can be reused for dspllaying ingredients ans user tables.

---
+++ frontend/src/components/shared/table-base/tableBase.jsx
@@ -0,0 +1,42 @@
+import "./table-base.css";
+
+export default function TableBase({
+  title,
+  description,
+  columns,
+  data,
+  renderRow,
+}) {
+  return (
+    <section className="table-wrapper">
+      {title && <h3 className="section-title">{title}</h3>}
+      {description && (
+        <p className="placeholder-text">{description}</p>
+      )}
+
+      <div className="table">
+        {/* HEADER */}
+        <div className="table__head">
+          {columns.map((col) => (
+            <span key={col.key}>{col.label}</span>
+          ))}
+          <span className="table__actions">Actions</span>
+        </div>
+
+        {/* ROWS */}
+        {data.map((item, index) =>
+          renderRow ? (
+            renderRow(item, index)
+          ) : (
+            <div className="table__row" key={item.id}>
+              {columns.map((col) => (
+                <span key={col.key}>{item[col.key]}</span>
+              ))}
+              <span className="table__actions" />
+            </div>
+          )
+        )}
+      </div>
+    </section>
+  );
+}
