c602deb8e050ab8b69123f5a01b7363c9fc76882
Kseniia Shlenskaia <xeniya.shlyonskaya@gmail.com>
2026-05-06 22:12:14 +0300

    Add ApiDoc documentation for backend controllers

--- backend/src/controllers/admin.controller.js
+++ backend/src/controllers/admin.controller.js
@@ -1,29 +1,9 @@
 import { placeholder } from '../utils/paceholder.js';
 
-/**
- * @api {get} /api/v1/adm/customers List customers (placeholder)
- * @apiName AdminListCustomersPlaceholder
- * @apiGroup AdminPlaceholders
- */
 export const listCustomers     = placeholder('admin.listCustomers');
 
 
 
-/**
- * @api {post} /api/v1/adm/ingredients Create ingredient (placeholder)
- * @apiName AdminCreateIngredientPlaceholder
- * @apiGroup AdminPlaceholders
- */
 export const createIngredient  = placeholder('admin.createIngredient');
-/**
- * @api {patch} /api/v1/adm/ingredients/:id Update ingredient (placeholder)
- * @apiName AdminUpdateIngredientPlaceholder
- * @apiGroup AdminPlaceholders
- */
 export const updateIngredient  = placeholder('admin.updateIngredient');
-/**
- * @api {delete} /api/v1/adm/ingredients/:id Delete ingredient (placeholder)
- * @apiName AdminDeleteIngredientPlaceholder
- * @apiGroup AdminPlaceholders
- */
 export const deleteIngredient  = placeholder('admin.deleteIngredient');
