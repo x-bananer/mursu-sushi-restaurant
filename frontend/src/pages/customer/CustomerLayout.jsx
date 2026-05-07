35a815abc6a1d1e508182e305db4064b8a988cbb
Kseniia Shlenskaia <xeniya.shlyonskaya@gmail.com>
2026-05-07 12:23:46 +0300

    Forbid admin from going to the user routes

--- frontend/src/pages/customer/CustomerLayout.jsx
+++ frontend/src/pages/customer/CustomerLayout.jsx
@@ -1,11 +1,18 @@
-import { Outlet } from "react-router";
+import { Navigate, Outlet } from "react-router";
 import Navbar from "../../components/customer/nav/Navbar";
 import BottomNav from "../../components/customer/nav/BottomNav";
 import Footer from "../../components/shared/footer/Footer";
 
 import { CartProvider } from "../../contexts/CartContext";
+import { useAuth } from "../../contexts/AuthContext";
 
 export default function CustomerLayout() {
+	const { user } = useAuth();
+
+	if (user?.role_id === 2) {
+		return <Navigate to="/adm" replace />;
+	}
+
 	return (
 		<CartProvider>
 			<header className="header">
